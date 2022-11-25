e1cdc51150918749173053299b88cc0b6df62fa3
Reggallucky <66362229+Reggallucky@users.noreply.github.com>
2022-11-26 06:26:53 +0700

    Update and rename README.md to tugas.java

--- README.md
+++
@@ -1,2 +0,0 @@
-# baru-tugas
-baru tugas
---
+++ tugas.java
@@ -0,0 +1,18 @@
+public class App{
+        String mobil;
+        String motor;
+        int jumlah;
+        public App(String string, String string2, int i) {
+        }
+        void mobil()
+        {
+            System.out.println("mobil saya"+mobil+"motor saya"+motor+"semua berjumlah"+jumlah);
+        }
+        class Jalankan{
+            
+            public static void main(String[] args) {
+                App akhdan = new App("toyota","ninja",2);
+                
+            }
+        }
+    }
